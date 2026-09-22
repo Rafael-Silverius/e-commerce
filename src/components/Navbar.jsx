d602994ad04452fc4fc6f142719ff6895c39226b
Rafael Walder <rafaelwalder99@gmail.com>
2026-09-22 12:37:57 +0300

    Added Vide Details

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
+import { useAuth } from "../context/useAuth";
 
 export default function Navbar() {
+  const { user, logout } = useAuth();
   return (
     <nav className="navbar">
       <div className="navbar-container">
@@ -12,18 +14,27 @@ export default function Navbar() {
             Home
           </Link>
           <Link to="/checkout" className="navbar-link">
-            Checkout
+            Cart
           </Link>
         </div>
         <div className="navbar-auth">
-          <div className="navbar-auth-links">
-            <Link to="/auth" className="btn btn-secondary">
-              Login
-            </Link>
-            <Link to="/auth" className="btn btn-primary">
-              Signup
-            </Link>
-          </div>
+          {!user ? (
+            <div className="navbar-auth-links">
+              <Link to="/auth" className="btn btn-secondary">
+                Login
+              </Link>
+              <Link to="/auth" className="btn btn-primary">
+                Signup
+              </Link>
+            </div>
+          ) : (
+            <div className="navbar-user">
+              <span>Hello, {user.email}</span>
+              <button className="btn btn-secondary" onClick={logout}>
+                Logout
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </nav>
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,13 +1,13 @@
-import { useContext, useState } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { AuthContext } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/useAuth";
 
 export default function Auth() {
   const [mode, setMode] = useState("signup");
   const [error, setError] = useState(null);
 
-  const { signUp, user, logout, login } = useContext(AuthContext);
+  const { signUp, login } = useAuth();
 
   const navigate = useNavigate();
 
@@ -25,15 +25,12 @@ export default function Auth() {
 
     if (result.success) navigate("/");
     else setError(result.error);
-    console.log(result);
   }
 
   return (
     <div>
       <div className="container">
         <div className="auth-container">
-          {user && <p>User logged in {user.email}</p>}
-          <button onClick={() => logout()}>Logout</button>
           <h1 className="page-title">
             {mode === "signup" ? "Sign Up" : "Login"}
           </h1>
